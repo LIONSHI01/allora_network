8ec423f76e04ce16a5bb888b0a1dca4ab8c705b5
lionc <shizijian2010@yahoo.com.hk>
2024-07-06 00:50:56 +0800

    update commands

--- allora_install.sh
+++ allora_install.sh
@@ -1,4 +1,31 @@
-git clone https://github.com/allora-network/allora-chain.git
-cd allora-chain
-docker compose pull
-docker compose up -d
+#!/bin/bash
+
+function install_node(){
+    git clone https://github.com/allora-network/allora-chain.git
+    cd allora-chain
+    docker compose pull
+    docker compose up -d
+}
+
+
+function inspect_logs(){
+    cd allora-chain
+    docker compose logs -f
+}
+
+function main_menu(){
+  while true; do
+      clear
+      echo "1. Install Node"
+      echo "2. View Logs"
+
+      case $OPTION in
+          1) install_node ;;
+          2) inspect_logs
+      esac
+      echo "Press any key back to menu..."
+      read -n 1
+  done
+}
+
+main_menu
